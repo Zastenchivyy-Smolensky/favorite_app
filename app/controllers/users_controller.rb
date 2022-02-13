25c62664c053daa83e414e4efd28d6a0aac2af3e
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-02-14 00:15:49 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  def show
+  end
+  def edit
+    @user = current_user
+  end
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path, notice: "ユーザ情報を更新しました"
+    else
+      render :edit
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
